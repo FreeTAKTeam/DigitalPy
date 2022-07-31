6f0cbf895e2e0ad1eba0b60daf9bbbf16d88bab2
naman108 <natha.paquette@gmail.com>
2022-07-31 12:27:42 -0300

    cotinued refactoring and updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='DigitalPy',
-      version='0.1',
+setup(name='digitalpy',
+      version='0.1.2',
       description="A python implementation of the aphrodite's framework",
       author='Natha Paquette',
       author_email='natha.paquette@gmail.com',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=find_packages('src'),
+      package_dir={"": "src"},
+      packages=find_packages(exclude=['tests', 'emergency_RI*']),
      )
