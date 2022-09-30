a08570a5ba925d03e113ab5840ba74026d460faa
naman108 <natha.paquette@gmail.com>
2022-09-30 16:45:53 -0300

    converted controller_message class to a proper interface using abstractmethods

--- digitalpy/routing/controller_message.py
+++ digitalpy/routing/controller_message.py
@@ -1,114 +1,77 @@
 from abc import ABC, abstractmethod
-
 class ControllerMessage(ABC):
     
-    def __init__(self):
-        self.sender = ""
-        self.context = ""
-        self.action = ""
-        self.values = {}
-        self.properties = None
-        self.errors = None
+    @abstractmethod
+    def get_id(self):
+        return self.id
     
+    @abstractmethod
+    def set_id(self, id):
+        self.id = id
+    
+    @abstractmethod
     def set_sender(self, sender):
         self.sender = sender
 
+    @abstractmethod
     def get_sender(self):
         return self.sender
     
+    @abstractmethod
     def set_context(self, context):
         self.context = context
     
+    @abstractmethod
     def get_context(self):
         return self.context
 
-    
+    @abstractmethod
     def get_action(self):
         return self.action
 
-    
+    @abstractmethod
     def set_action(self, action):
         self.action = action
     
-    
+    @abstractmethod
     def set_value(self, name, value):
         """Set a value
         @param name The name of the variable
         @param value The value of the variable
         """
-        self.values[name] = value
-    #
-    # Set all key value pairs at once
-    # @param values The associative array
-    #/
     
+    @abstractmethod
     def set_values(self, values: dict):
-        self.values = values
+        """set all key value pairs at once"""
     
+    @abstractmethod
     def get_values(self):
-        return self.values
+        """get all key value pairs at once"""
     
-    #
-    # Get a value
-    # @param name The name of the variable
-    # @param default The default value if the value is not defined or invalid while exceptions are suppressed (optional, default: _null_)
-    # @param validateDesc An validation description to be used with Validator::validate() (optional, default: _null_)
-    # @param suppressException Boolean whether to suppress a validation exception or not (optional, default: _false_)
-    # @return The (filtered) value or default, if it does not exist
-    #/
-    
+    @abstractmethod
     def get_value(self, name, default=None):
-        return self.values.get(name, default)
-    #
-    # Get a value as boolean
-    # @param name The name of the variable
-    # @param default The default value if the value is not defined (default: _false_)
-    # @return The value or null if it does not exist
-    #/
+        """Get a value"""
     
-    def get_boolean_value(name, default=False):
-        raise NotImplementedError
-    #
-    # Get all key value pairs
-    # @return An associative array
-    #/
-    
-    def get_values(self):
-        return self.values
-    #
-    # Remove a value
-    # @param name The name of the variable
-    #/
-    
+    @abstractmethod
+    def get_boolean_value(self, name, default=False):
+        """Get a value as boolean"""
+
+    @abstractmethod
     def clear_value(self, name):
-        del self.values[name]
-    #
-    # Remove all values
-    #/
-    
+        """Remove a value"""
+        
+    @abstractmethod
     def clear_values(self):
-        self.values = {}
-    #
-    # Check for existence of a value
-    # @param name The name of the variable
-    # @return Boolean whether the value exists or not exist
-    #/
-    
-    def has_value(name):
-        raise NotImplementedError
-    #
-    # Set a property
-    # @param name The name of the property
-    # @param value The value of the property
-    #/
-    
-    def set_property(name, value):
-        raise NotImplementedError
-    #
-    # Get a property
-    # @param name The name of the property
-    # @return The property value or null
-    #/
-    
-    def get_property(name):
-        raise NotImplementedError
+        """Remove all values"""
+        
+    @abstractmethod
+    def has_value(self, name):
+        """Check for existence of a value"""
+        
+    @abstractmethod
+    def set_property(self, name, value):
+        """Set a property"""
+        
+    @abstractmethod
+    def get_property(self, name):
+        """Get a property"""
