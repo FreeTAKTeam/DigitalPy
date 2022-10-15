c149c1dd6f1bff6537fbf2c84bff21dbeb37c250
naman108 <natha.paquette@gmail.com>
2022-10-15 14:54:15 -0400

    updated to reset the context per request instead of reusing the same context which caused errors

--- digitalpy/routing/impl/async_action_mapper.py
+++ digitalpy/routing/impl/async_action_mapper.py
@@ -1,3 +1,4 @@
+import time
 from digitalpy.config.action_key import ActionKey
 from digitalpy.model.application_event import ApplicationEvent
 from digitalpy.config.impl.config_action_key_provider import ConfigActionKeyProvider
@@ -8,7 +9,7 @@ from digitalpy.routing.response import Response
 from digitalpy.routing.action_mapper import ActionMapper
 from digitalpy.parsing.formatter import Formatter
 from digitalpy.core.object_factory import ObjectFactory
-
+import threading
 import zmq
 
 
@@ -36,6 +37,13 @@ class AsyncActionMapper(ActionMapper):
         self.routing_subscriber_address = routing_subscriber_address
         self.formatter = formatter
         self.initiate_sockets()
+
+    def get_routing_publisher(self):
+        sock_context = zmq.Context()
+        routing_publisher = sock_context.socket(zmq.PUB)
+        routing_publisher.connect(self.routing_subscriber_address)
+        time.sleep(1)
+        return routing_publisher
 
     def initiate_sockets(self):
         self.sock_context = zmq.Context()
@@ -83,15 +91,21 @@ class AsyncActionMapper(ActionMapper):
         self, response: Response, request: Request, listener: zmq.Socket, timeout=10000
     ):
         try:
+            topic = f"/routing/response/{request.get_sender()}/{request.get_context()}/{request.get_action()}/{request.get_format()}/{request.get_id()}"
             listener.RCVTIMEO = timeout
             messages = listener.recv_multipart()
-            topic = f"/routing/response/{request.get_sender()}/{request.get_context()}/{request.get_action()}/{request.get_format()}/{request.get_id()}"
             listener.setsockopt_string(zmq.UNSUBSCRIBE, topic)
             values = messages[1]
             response.set_values(values)
             self.formatter.deserialize(response)
             return response
         except Exception as e:
+            listener.setsockopt_string(zmq.UNSUBSCRIBE, topic)
+            # temporary fix to see if delay is required before closing the socket
+            import time
+            time.sleep(3)
+            print(e)
+            listener.close()
             pass
 
     #
@@ -116,23 +130,31 @@ class AsyncActionMapper(ActionMapper):
 
         if return_listener:
             topic = f"/routing/response/{request.get_sender()}/{request.get_context()}/{request.get_action()}/{request.get_format()}/{request.get_id()}"
-            routing_subscriber = self.sock_context.socket(zmq.SUB)
+            routing_subscriber = zmq.Context().socket(zmq.SUB)
             routing_subscriber.connect(self.routing_publisher_address)
             routing_subscriber.setsockopt_string(zmq.SUBSCRIBE, topic)
         else:
             routing_subscriber = None
-        self.routing_publisher.send_multipart(
-            [
-                f"/routing/request/{request.get_sender()}/{request.get_context()}/{request.get_action()}/{request.get_format()}/{request.get_id()}".encode(
-                    "utf-8"
-                ),
-                request.get_values(),
-            ]
-        )
+
+        threading.Thread(target = self.submit_request, args=(request,).start())
 
         return routing_subscriber
 
+    def submit_request(self, request):
+        with self.get_routing_publisher() as routing_publisher:
+            routing_publisher.send_multipart(
+                [
+                    f"/routing/request/{request.get_sender()}/{request.get_context()}/{request.get_action()}/{request.get_format()}/{request.get_id()}".encode(
+                        "utf-8"
+                    ),
+                    request.get_values(),
+                ]
+            )
+
     def __getstate__(self):
+        self.routing_publisher.close()
+        self.general_routing_subscriber.close()
+        self.sock_context.term()
         state = self.__dict__.copy()
         del state["routing_publisher"]
         del state["general_routing_subscriber"]
