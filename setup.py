8517f2283305a62dc0c82af42319af0c635ee6f8
naman108 <natha.paquette@gmail.com>
2023-02-06 11:29:48 -0400

    fixed typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='digitalpy',
-      version='0.3.8,
+      version='0.3.8',
       description="A python implementation of the aphrodite's specification, heavily based on WCMF",
       author='Natha Paquette',
       author_email='natha.paquette@gmail.com',
