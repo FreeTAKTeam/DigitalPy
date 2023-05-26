d549c37cf4a218feaa9e81df0f002b6f7fe3096f
naman108 <natha.paquette@gmail.com>
2023-05-26 19:53:26 -0300

    improved error handling

--- digitalpy/core/digipy_configuration/impl/inifile_configuration.py
+++ digitalpy/core/digipy_configuration/impl/inifile_configuration.py
@@ -105,17 +105,20 @@ class InifileConfiguration(Configuration):
         with open(filename) as f:
             lines = f.readlines()
             for line in lines:
-                line = line.strip()
-                if line == "" or line[0] == ";":
-                    continue
-                if line.startswith("[") and line.endswith("]"):
-                    section_name = line[1 : len(line) - 1]
-                    config_array[section_name] = {}
-                else:
-                    parts = line.split("=", 1)
-                    key = parts[0].strip()
-                    value = parts[1].strip()
-                    config_array[section_name][key] = value
+                try:
+                    line = line.strip()
+                    if line == "" or line[0] == ";":
+                        continue
+                    if line.startswith("[") and line.endswith("]"):
+                        section_name = line[1 : len(line) - 1]
+                        config_array[section_name] = {}
+                    else:
+                        parts = line.split("=", 1)
+                        key = parts[0].strip()
+                        value = parts[1].strip()
+                        config_array[section_name][key] = value
+                except Exception as ex:
+                    raise ValueError(f"line: {line} in file: {filename} is invalid with error: {str(ex)}") from ex
 
         return config_array
 
