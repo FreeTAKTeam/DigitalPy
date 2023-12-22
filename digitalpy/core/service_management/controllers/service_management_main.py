ab04a93542e97811105c08e1b79d44a73941dcc2
naman108 <natha.paquette@gmail.com>
2023-12-22 12:39:16 -0400

    added service stop support and default service status

--- digitalpy/core/service_management/controllers/service_management_main.py
+++ digitalpy/core/service_management/controllers/service_management_main.py
@@ -1,22 +1,24 @@
+"""This module is used to manage the services running on the zmanager"""
 from typing import Dict, List
-from digitalpy.core.component_management.impl.component_registration_handler import ComponentRegistrationHandler
 
 from digitalpy.core.main.impl.default_factory import DefaultFactory
 from digitalpy.core.zmanager.request import Request
-from digitalpy.core.main.factory import Factory
 from digitalpy.core.main.object_factory import ObjectFactory
 from digitalpy.core.service_management.domain.service_status import ServiceStatus
 from digitalpy.core.telemetry.tracing_provider import TracingProvider
-from digitalpy.core.service_management.controllers.service_management_process_controller import ServiceManagementProcessController
-from digitalpy.core.service_management.digitalpy_service import DigitalPyService, COMMAND_PROTOCOL, COMMAND_ACTION
+from digitalpy.core.service_management.controllers.service_management_process_controller \
+    import ServiceManagementProcessController
+from digitalpy.core.service_management.digitalpy_service \
+    import DigitalPyService, COMMAND_PROTOCOL, COMMAND_ACTION
 from digitalpy.core.parsing.formatter import Formatter
 from digitalpy.core.service_management.domain.service_description import ServiceDescription
 from digitalpy.core.digipy_configuration.configuration import Configuration
 from digitalpy.core.zmanager.response import Response
 
+
 class ServiceManagementMain(DigitalPyService):
     # TODO: this class' description should come from the ASOT
-    """"""
+    """a class used to manage the services running on the zmanager"""
 
     def __init__(
         self,
@@ -48,27 +50,55 @@ class ServiceManagementMain(DigitalPyService):
             integration_manager_protocol,
             formatter,
             protocol=COMMAND_PROTOCOL,
-            network=None # type: ignore
+            network=None  # type: ignore
         )
 
         # the service index is used to keep track of all registered services, their states, and their configurations
         self._service_index: Dict[str, ServiceDescription] = {}
         self.component_index: Dict[str, Configuration] = {}
+        self.process_controller: ServiceManagementProcessController
 
     def initialize_controllers(self):
         """This method is used to initialize the controllers once the service is started"""
-        self.process_controller: ServiceManagementProcessController = ObjectFactory.get_instance("ServiceManagerProcessController")
+        self.process_controller: ServiceManagementProcessController = ObjectFactory.get_instance(
+            "ServiceManagerProcessController")
+
+    def stop(self):
+        """This method is used to stop the service"""
+        self.stop_all_services()
+        raise SystemExit
+
+    def stop_all_services(self):
+        """This method is used to stop all services"""
+        for service_id in self._service_index:
+            self.stop_service(service_id)
 
     def start(self, object_factory: DefaultFactory, tracing_provider: TracingProvider, component_index: Dict[str, Configuration]):
         """This method is used to start the service"""
         self.initialize_connections(COMMAND_PROTOCOL)
         self.component_index = component_index
         object_factory.clear_instance("servicemanager")
-        super().start(object_factory=object_factory, tracing_provider=tracing_provider)
-        
-    def stop(self):
-        """This method is used to stop the service"""
-        super().stop()
+
+        ObjectFactory.configure(object_factory)
+        self.tracer = tracing_provider.create_tracer(self.service_id)
+        self.initialize_controllers()
+        self.initialize_connections(self.protocol)
+
+        for component_name in self.component_index:
+            component_configuration = self.component_index[component_name]
+            for service_name in component_configuration.get_sections():
+                # TODO: move service definitions out of component manifest
+                if not service_name.endswith("Service"):
+                    continue
+                service_config = component_configuration.get_section(
+                    service_name)
+                if service_config.get("default_status", ServiceStatus.STOPPED) == ServiceStatus.RUNNING.value:
+                    service_id = str(service_config.get("service_id"))
+                    self.start_service(service_id)
+
+        self.status = ServiceStatus.RUNNING
+        # member exists in parent class
+        self.execute_main_loop()  # pylint: disable=no-member
 
     def event_loop(self):
         """This method is used to run the service"""
@@ -78,8 +108,8 @@ class ServiceManagementMain(DigitalPyService):
             self.handle_command(command)
 
     def handle_exception(self, exception: Exception):
-
-        #TODO: add logic to handle exceptions
+        """This method is used to handle an exception"""
+        # TODO: add logic to handle exceptions
         print(exception)
 
     def handle_command(self, command: Response):
@@ -105,7 +135,7 @@ class ServiceManagementMain(DigitalPyService):
         """
         responses = self.broker_receive()
         return responses
-    
+
     def initialize_service_description(self, service_configuration: dict, service_id: str) -> ServiceDescription:
         """This method is used to initialize a service description"""
         # construct the service description
@@ -116,11 +146,15 @@ class ServiceManagementMain(DigitalPyService):
 
         # add the values from the configuration to the service description
         new_service_instance.name = str(service_configuration.get("name"))
-        new_service_instance.protocol = str(service_configuration.get("protocol"))
-        new_service_instance.network_interface = str(service_configuration.get("network"))
+        new_service_instance.protocol = str(
+            service_configuration.get("protocol"))
+        new_service_instance.network_interface = str(
+            service_configuration.get("network"))
         new_service_instance.status = ServiceStatus.STOPPED
-        new_service_instance.description = str(service_configuration.get("description"))
-        new_service_instance.port = int(service_configuration.get("port")) # type: ignore
+        new_service_instance.description = str(
+            service_configuration.get("description"))
+        new_service_instance.port = int(
+            service_configuration.get("port"))  # type: ignore
         new_service_instance.host = str(service_configuration.get("host"))
 
         # add the service description to the service index
@@ -134,7 +168,7 @@ class ServiceManagementMain(DigitalPyService):
         # check if the service is already running
         if self.is_service_running(service_id):
             return
-        
+
         # parse the service id into the component name and the service name
         component_name, service_name = service_id.split(".")
 
@@ -142,16 +176,20 @@ class ServiceManagementMain(DigitalPyService):
         service_component_configuration = self.component_index[component_name]
 
         # get the service configuration from the manifest of the component
-        service_configuration = service_component_configuration.get_section(service_name)
+        service_configuration = service_component_configuration.get_section(
+            service_name)
 
-        service_description = self.initialize_service_description(service_configuration, service_id)
+        service_description = self.initialize_service_description(
+            service_configuration, service_id)
 
-        service_class = self.initialize_service_class(service_configuration, service_description)
+        service_class = self.initialize_service_class(
+            service_configuration, service_description)
 
-        self.process_controller.start_process(service_description, service_class)
+        self.process_controller.start_process(
+            service_description, service_class)
 
         service_description.status = ServiceStatus.RUNNING
-        
+
     def is_service_running(self, service_id: str) -> bool:
         """This method is used to check if a service is running"""
         if service_id not in self._service_index:
@@ -159,17 +197,22 @@ class ServiceManagementMain(DigitalPyService):
         return self._service_index[service_id].status == ServiceStatus.RUNNING
 
     def initialize_service_class(self, service_configuration: dict, service_description: ServiceDescription):
-        base_config: Configuration = ObjectFactory.get_instance("configuration")
+        base_config: Configuration = ObjectFactory.get_instance(
+            "configuration")
 
         service_configuration.update(base_config.get_section("Service"))
 
         # initialize the service class
-        service_class: DigitalPyService = ObjectFactory.get_instance(service_description.name, service_configuration)
-        
+        service_class: DigitalPyService = ObjectFactory.get_instance(
+            service_description.name, service_configuration)
+
         return service_class
 
     def stop_service(self, service_id: str):
         """This method is used to stop a service"""
+        # handle a system shutdown
+        if service_id.lower() == self.service_id.lower():
+            self.stop()
 
         service_description = self._service_index[service_id]
 
