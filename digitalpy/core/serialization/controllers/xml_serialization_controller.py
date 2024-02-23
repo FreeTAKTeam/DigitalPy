b8c96c9cf743dfd7d6cd898c1872565a32c43681
naman108 <natha.paquette@gmail.com>
2024-02-23 17:51:12 -0400

    added protocol deserialization support

--- digitalpy/core/serialization/controllers/xml_serialization_controller.py
+++ digitalpy/core/serialization/controllers/xml_serialization_controller.py
@@ -2,12 +2,126 @@ from typing import Union
 from digitalpy.core.main.controller import (
     Controller,
 )
-from digitalpy.core.domain.node import Node
 from copy import deepcopy
 from lxml.etree import Element  # pylint: disable=no-name-in-module
 from lxml import etree
 import xmltodict
+import uuid
 
+from digitalpy.core.domain.node import Node
+from digitalpy.core.parsing.load_configuration import ModelConfiguration as LoadConf
+from digitalpy.core.main.object_factory import ObjectFactory
+
+class XMLDeserializer:
+    """this class exposes the basic lxml target interface for parsing COTs to Node objects"""
+    def __init__(self, node: Node) -> None:
+        if node is None:
+            raise ValueError("node cannot be None")
+        if not isinstance(node, Node):
+            raise ValueError("node must be an instance of Node")
+        self.root_node: Node = node
+        self.curr_object: Node = None
+        self.prev_object: Node = None
+        self.siblings = []
+        self.stack = []
+        
+        self.xml_obj: etree.ElementBase = None
+        self.xml_root: etree.ElementBase = None
+
+    def start(self, tag: str, attrib: dict):
+        """this method is called when the parser encounters a start tag
+
+        Args:
+            tag (str): the tag name
+            attrib (dict): the attributes of the tag
+        """
+        # check that the tag matches the current node
+        if self.curr_object is None:
+            self.curr_object = self.root_node
+        else:
+            children = self._get_child_node(tag)
+
+            # if the tag is a sibling of the current node, set the current node to the sibling
+            if len(children)>1:
+                self.siblings = children
+                self.curr_object = self.siblings.pop()
+
+            # if the tag is a child of the current node, set the current node to the child
+            elif len(children)==1:
+                self.curr_object = children.pop()
+            
+            # if the tag is not a child of the current node, create a new xml element and add it to the current node
+            else:
+                if self.xml_obj is None:
+                    self.xml_root = etree.Element(tag, attrib)
+                    self.xml_obj = self.xml_root
+                else:
+                    self.xml_obj = etree.SubElement(self.xml_obj, tag, attrib)
+                return
+        
+        # if the current node is the same type as the previous node, create a new instance of the current node
+        if type(self.curr_object) == type(self.prev_object):
+            id = str(uuid.uuid1())
+            oid = ObjectFactory.get_instance(
+                "ObjectId", {"id": id, "type": str(type(self.curr_object))}
+            )
+
+            new_inst = type(self.curr_object)(LoadConf(), None, oid)
+            self.prev_object.get_parent().add_child(new_inst)
+            self.curr_object = new_inst
+
+        # set the attributes of the current node
+        for key, value in attrib.items():
+            if key in self.curr_object.cot_attributes:
+                setattr(self.curr_object, key, value)
+            else:
+                self.curr_object.xml.set(key, value)
+
+    def _get_child_node(self, tag: str) -> list[Node]:
+        """gets the child node of the current node with the given tag name
+
+        Args:
+            tag (str): the tag name of the child node
+
+        Returns:
+            Node: the child node
+        """
+        children = self.curr_object.get_children_ex(children_type=tag)
+        return children
+        
+    def end(self, tag):
+        """this method is called when the parser encounters an end tag
+
+        Args:
+            tag (str): the tag name, required by the lxml target interface
+        """
+        if self.xml_root is not None and tag == self.xml_root.tag:
+            self.xml_obj = None
+        elif self.xml_obj is not None:
+            self.xml_obj = self.xml_obj.getparent()
+        else:
+            self.prev_object = self.curr_object
+            self.curr_object = self.curr_object.get_parent()
+        
+    def data(self, data: str):
+        """this method is called when the parser encounters data
+
+        Args:
+            data (str): the data
+        """
+
+        # if the current node has an xml object, set the text of the xml object
+        if self.xml_obj is not None:
+            self.xml_obj.text = data
+        # otherwise, set the text of the current node
+        else:
+            self.curr_object.text = data
+
+    def comment(self, text):
+        pass
+
+    def close(self):
+        return self.root_node
 
 class XMLSerializationController(Controller):
     def __init__(self, request, response, action_mapper, configuration) -> None:
@@ -26,6 +140,21 @@ class XMLSerializationController(Controller):
         # TODO this should not be a direct return and should be fixed ASAP
         return self._serialize_node(node, node.__class__.__name__.lower())
 
+    def deserialize(self, message: bytes, model_object: Node, *args, **kwargs):
+        """converts the provided xml string to a node
+
+        Args:
+            message (bytes): the xml string to be converted to a node
+        """
+        return self._deserialize(message, model_object)
+    
+    def _deserialize(self, message: bytes, model_object: Node) -> Node:
+        target = XMLDeserializer(model_object)
+        parser = etree.XMLParser(target=target)
+        result = etree.XML(message, parser)
+        self.response.set_value("model_object", result)
+        return result
+    
     def _serialize_node(
         self, node: Node, tag_name: str, level=0
     ) -> Union[str, Element]:
--- digitalpy/core/serialization/serialization_facade.py
+++ digitalpy/core/serialization/serialization_facade.py
@@ -11,17 +11,19 @@ from .configuration.serialization_constants import (
     LOG_FILE_PATH
 )
 from . import base
+
+
 class Serialization(DefaultFacade):
     """Facade class for the this component. Responsible for handling all public
     routing. Forwards all requests to the internal router.
       WHY
       <ul>
-      	<li><b>Isolation</b>: We can easily isolate our code from the complexity of
+        <li><b>Isolation</b>: We can easily isolate our code from the complexity of
     a subsystem.</li>
-      	<li><b>Testing Process</b>: Using Facade Method makes the process of testing
+        <li><b>Testing Process</b>: Using Facade Method makes the process of testing
     comparatively easy since it has convenient methods for common testing tasks.
     </li>
-      	<li><b>Loose Coupling</b>: Availability of loose coupling between the
+        <li><b>Loose Coupling</b>: Availability of loose coupling between the
     clients and the Subsystems.</li>
       </ul>
     """
@@ -35,39 +37,41 @@ class Serialization(DefaultFacade):
             log_file_path: str = LOG_FILE_PATH,
     ):
         super().__init__(
-                # the path to the external action mapping
-                action_mapping_path=ACTION_MAPPING_PATH,
-                # the path to the internal action mapping
-                internal_action_mapping_path=INTERNAL_ACTION_MAPPING_PATH,
-                # the path to the logger configuration
-                logger_configuration=LOGGING_CONFIGURATION_PATH,
-                # the package containing the base classes
-                base=base,
-                # the component specific action mapper (passed by constructor)
-                action_mapper=serialization_action_mapper,
-                # the request object (passed by constructor)
-                request=request,
-                # the response object (passed by constructor)
-                response=response,
-                # the configuration object (passed by constructor)
-                configuration=configuration,
-                # the template for the absolute path to the model object definitions
-                configuration_path_template=CONFIGURATION_PATH_TEMPLATE,
-                # the path to the manifest file
-                manifest_path=MANIFEST_PATH,
-                # the path for log files to be stored
-                log_file_path=log_file_path
+            # the path to the external action mapping
+            action_mapping_path=ACTION_MAPPING_PATH,
+            # the path to the internal action mapping
+            internal_action_mapping_path=INTERNAL_ACTION_MAPPING_PATH,
+            # the path to the logger configuration
+            logger_configuration=LOGGING_CONFIGURATION_PATH,
+            # the package containing the base classes
+            base=base,
+            # the component specific action mapper (passed by constructor)
+            action_mapper=serialization_action_mapper,
+            # the request object (passed by constructor)
+            request=request,
+            # the response object (passed by constructor)
+            response=response,
+            # the configuration object (passed by constructor)
+            configuration=configuration,
+            # the template for the absolute path to the model object definitions
+            configuration_path_template=CONFIGURATION_PATH_TEMPLATE,
+            # the path to the manifest file
+            manifest_path=MANIFEST_PATH,
+            # the path for log files to be stored
+            log_file_path=log_file_path
         )
-        #self.deserialize = SerializationGeneralController()
-        #self.serialize_to_file = SerializationGeneralController()
-        #self.serialize = SerializationGeneralController()
-        #self.domain_to_xml_parsing = SerializationGeneralController()
-        #self.xml_to_domain_parsing = SerializationGeneralController()
-        #self.domain_to_json_parsing = SerializationGeneralController()
-        #self.domain_to_protobuf_parsing = SerializationGeneralController()
-        #self.deserialize_from_file = SerializationGeneralController()
-        self.xml_serializer = XMLSerializationController(request, response, serialization_action_mapper, configuration)
-        self.general_serializer = SerializationGeneralController(request, response, serialization_action_mapper, configuration)
+        # self.deserialize = SerializationGeneralController()
+        # self.serialize_to_file = SerializationGeneralController()
+        # self.serialize = SerializationGeneralController()
+        # self.domain_to_xml_parsing = SerializationGeneralController()
+        # self.xml_to_domain_parsing = SerializationGeneralController()
+        # self.domain_to_json_parsing = SerializationGeneralController()
+        # self.domain_to_protobuf_parsing = SerializationGeneralController()
+        # self.deserialize_from_file = SerializationGeneralController()
+        self.xml_serializer = XMLSerializationController(
+            request, response, serialization_action_mapper, configuration)
+        self.general_serializer = SerializationGeneralController(
+            request, response, serialization_action_mapper, configuration)
 
     def initialize(self, request, response):
         super().initialize(request, response)
@@ -91,4 +95,8 @@ class Serialization(DefaultFacade):
         """serialize a node to a given protocol
         """
         self.general_serializer.serialize_node_to_protocol(*args, **kwargs)
-    
+
+    def desearialize_protocol_to_node(self, *args, **kwargs):
+        """serialize a protocol to a node
+        """
+        self.general_serializer.deserialize_protocol_to_node(*args, **kwargs)
