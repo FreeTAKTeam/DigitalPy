a2618f5453d72daba4425f7516bf20cde3dd45c3
naman108 <natha.paquette@gmail.com>
2024-05-09 19:47:13 -0300

    updated all tables to use 2.x mapping

--- digitalpy/core/IAM/persistence/iam_base.py
+++ digitalpy/core/IAM/persistence/iam_base.py
@@ -1,3 +1,5 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-IAMBase = declarative_base()
+
+class IAMBase(DeclarativeBase):
+    pass
--- digitalpy/core/IAM/persistence/system_group.py
+++ digitalpy/core/IAM/persistence/system_group.py
@@ -1,8 +1,8 @@
 """this file contains the persistency class for the system group table"""
 
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Optional
 from sqlalchemy import Text, Column
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 if TYPE_CHECKING:
     from digitalpy.core.IAM.persistence.system_group_permission import SystemGroupPermission
@@ -20,14 +20,14 @@ class SystemGroup(IAMBase):
     """
 
     __tablename__ = 'SystemGroup'
-    uid = Column(Text, primary_key=True)
-    name = Column(Text, nullable=True)
-    description = Column(Text, nullable=True)
+    uid: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[Optional[str]]
+    description: Mapped[Optional[str]]
 
     # relationships
-    system_group_permissions: List['SystemGroupPermission'] = relationship("SystemGroupPermission", back_populates="system_groups")
+    system_group_permissions: Mapped[List['SystemGroupPermission']] = relationship(back_populates="system_groups")
 
-    system_user_groups: List['SystemUserGroups'] = relationship("SystemUserGroups", back_populates="system_groups")
+    system_user_groups: Mapped[List['SystemUserGroups']] = relationship(back_populates="system_groups")
 
     def __repr__(self) -> str:
         return super().__repr__() + f"uid={self.uid}, name={self.name}, description={self.description}"
