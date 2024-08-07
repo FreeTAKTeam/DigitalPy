3e7778dec95c7ac30428ccc3687bef7b69e1e91a
naman108 <natha.paquette@gmail.com>
2024-08-07 16:38:58 -0300

    fix: updated test to use ActionKeyController

--- tests/test_zmanager/functional/test_default_routing_worker.py
+++ tests/test_zmanager/functional/test_default_routing_worker.py
@@ -3,6 +3,7 @@ import time
 from unittest.mock import patch
 import pytest
 
+from digitalpy.core.digipy_configuration.action_key_controller import ActionKeyController
 from digitalpy.core.main.object_factory import ObjectFactory
 from digitalpy.core.zmanager.configuration.zmanager_constants import ZMANAGER_MESSAGE_DELIMITER
 from digitalpy.core.zmanager.impl.default_routing_worker import DefaultRoutingWorker
@@ -27,16 +28,19 @@ def test_integration_manager_subscription(mock_process_integration_manager_messa
     time.sleep(0.5)
 
     # send a message to the integration manager
-    message_topic = "routing_worker"
+    ak_controller: ActionKeyController = ObjectFactory.get_instance("ActionKeyController")
+    ak = ak_controller.new_action_key()
+    ak.decorator = "ROUTING_WORKER"
     message_content = "content"
-    message = message_topic + ZMANAGER_MESSAGE_DELIMITER.decode() + message_content
+    message = ak_controller.serialize_to_topic(ak) + ZMANAGER_MESSAGE_DELIMITER.decode() + message_content
     zmanager.send_integration_manager_message(message)
 
     # wait for the worker to process the message
-    time.sleep(0.5)
+    time.sleep(3)
 
     # check that the worker received processed the message correctly
-    mock_process_integration_manager_message.assert_called_with([message_content.encode()])
+    assert mock_process_integration_manager_message.called
+    mock_process_integration_manager_message.assert_called_with(message)
 
     # stop the zmanager
     zmanager.stop()
