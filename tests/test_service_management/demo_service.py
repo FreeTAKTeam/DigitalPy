6bee072bf1f8f21307fc080bba991541f388afdb
naman108 <natha.paquette@gmail.com>
2024-08-08 11:45:55 -0300

    test: updated demo service in accordance with DigitalPyService changes

--- tests/test_service_management/demo_service.py
+++ tests/test_service_management/demo_service.py
@@ -1,5 +1,6 @@
 import time
-from digitalpy.core.parsing.formatter import Formatter
+from digitalpy.core.zmanager.impl.integration_manager_subscriber import IntegrationManagerSubscriber
+from digitalpy.core.zmanager.impl.subject_pusher import SubjectPusher
 from digitalpy.core.service_management.domain.service import Service
 from digitalpy.core.service_management.digitalpy_service import DigitalPyService
 
@@ -7,8 +8,8 @@ from digitalpy.core.service_management.digitalpy_service import DigitalPyService
 class DemoService(DigitalPyService):
     """Demo service class."""
 
-    def __init__(self, formatter: Formatter, service: Service):
-        super().__init__(service_id="demo_service", formatter=formatter, service=service)
+    def __init__(self, service: Service, subject_pusher: SubjectPusher, integration_manager_subscriber: IntegrationManagerSubscriber):
+        super().__init__(service_id="demo_service", service=service, subject_pusher=subject_pusher, integration_manager_subscriber=integration_manager_subscriber)
 
     def initialize(self):
         """Initialize the service."""
