d29552d0173f3c8b5b0ec040acf150b7d24306dc
naman108 <natha.paquette@gmail.com>
2024-10-17 19:53:38 -0300

    fix: added missing ASYNC_Decorator def

--- digitalpy/core/zmanager/configuration/zmanager_constants.py
+++ digitalpy/core/zmanager/configuration/zmanager_constants.py
@@ -16,6 +16,13 @@ ZMANAGER_MESSAGE_FORMAT = "pickled"
 TOPIC = "Topic"
 DEFAULT_ENCODING = "utf-8"
 
+# Signals that a message should be sent directly to the integration manager to be published
+# bypasing the default routing worker
 PUBLISH_DECORATOR = "PUBLISH"
+# Signals that the action should not be executed sequentially and should instead be initiated
+# by some response. This can be in conjunction with the prev_flow key when a sub-flow
+# is called as part of the fulfillment of an action and we're not synchronously waiting for
+# a response
+ASYNC_DECORATOR = "ASYNC"
 
 RESPONSE = "RESPONSE"
