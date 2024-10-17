b2cd31df8fa04831f5fd7fcbb9fb3921243f4000
naman108 <natha.paquette@gmail.com>
2024-10-17 08:23:00 -0300

    feat: added action key builder

--- digitalpy/core/digipy_configuration/configuration/digipy_configuration_constants.py
+++ digitalpy/core/digipy_configuration/configuration/digipy_configuration_constants.py
@@ -1,2 +1,19 @@
+from string import Template
+import pathlib
+
 ACTION_MAPPING_SECTION = 'actionmapping'
 MAX_FLOW_LENGTH = 100
+ACTION_KEY = 'ActionKey'
+
+CONFIGURATION_FORMAT = "json"
+
+CURRENT_COMPONENT_PATH = pathlib.Path(__file__).parent.parent.absolute()
+
+CONFIGURATION_PATH_TEMPLATE = Template(
+    str(
+        pathlib.PurePath(
+            CURRENT_COMPONENT_PATH, "configuration/model_definitions/$message_type"
+        )
+    )
+    + f".{CONFIGURATION_FORMAT}"
+)
