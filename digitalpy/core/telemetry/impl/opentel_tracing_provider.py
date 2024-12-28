9e68873f0967c108194326bd5c9c6d3f05b5f15b
naman108 <natha.paquette@gmail.com>
2024-12-28 15:24:13 -0400

    chore: reformatted file

--- digitalpy/core/telemetry/impl/opentel_tracing_provider.py
+++ digitalpy/core/telemetry/impl/opentel_tracing_provider.py
@@ -2,7 +2,10 @@ from digitalpy.core.telemetry.impl.opentel_tracer import OpenTelTracer
 from digitalpy.core.telemetry.tracing_provider import TracingProvider
 from opentelemetry.sdk.trace import TracerProvider
 from digitalpy.core.main.object_factory import ObjectFactory
-from digitalpy.core.telemetry.impl.opentel_tracing_exporter import OpenTelTracingExporter
+from digitalpy.core.telemetry.impl.opentel_tracing_exporter import (
+    OpenTelTracingExporter,
+)
+
 
 class OpenTelTracingProvider(TracingProvider):
     """tracing provider implementation for the open telemetry protocol."""
@@ -11,9 +14,11 @@ class OpenTelTracingProvider(TracingProvider):
         self.provider = TracerProvider()
         exporter = ObjectFactory.get_new_instance("TracerExporter")
         self.exporter = OpenTelTracingExporter(exporter)
-        self.processor = ObjectFactory.get_new_instance("TracerProcessor", dynamic_configuration={"span_exporter": exporter})
+        self.processor = ObjectFactory.get_new_instance(
+            "TracerProcessor", dynamic_configuration={"span_exporter": exporter}
+        )
         self.provider.add_span_processor(self.processor)
-        
+
     def create_tracer(self, tracer_name: str) -> OpenTelTracer:
         """create a new OpenTelTracer instance
 
@@ -24,7 +29,7 @@ class OpenTelTracingProvider(TracingProvider):
             OpenTelTracer: an open telemetry tracer instance
                 from the current provider instance with the passed name
         """
-        if not hasattr(self, 'provider'):
+        if not hasattr(self, "provider"):
             self.initialize_tracing()
         return OpenTelTracer(self.provider.get_tracer(tracer_name))
 
